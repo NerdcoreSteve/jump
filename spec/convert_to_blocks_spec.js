40c76da39a1dee7b046c0138190360bac2623ef0
Steven Smith <stevensmith@Stevens-Air.netgear.com>
2016-05-15 20:54:46 -0600

    added two tests, one passing, one failing

--- spec/convert_to_blocks_spec.js
+++ spec/convert_to_blocks_spec.js
@@ -5,10 +5,29 @@ describe('convert_to_blocks', () => {
     var char_key = require('../front-end/js/char_key.js')
 
     it('should return an array of objects the same length of the given string',
-        () => expect(R.map((x) => typeof x, convert_to_blocks(char_key, '@ +^')))
+        () => expect(R.map((block) => typeof block, convert_to_blocks(char_key, '@ +^')))
             .toEqual(['object', 'object', 'object', 'object']))
 
     it('should return an array of objects with color, type, and position properties',
-        () => expect(R.all((x) => false, convert_to_blocks(char_key, '@ +^')))
-            .toEqual(true))
+        () => expect(
+            R.all((block) =>
+                R.has('color', block) && R.has('type', block) && R.has('position', block),
+                convert_to_blocks(char_key, '@ +^')))
+                    .toEqual(true))
+
+    it('should return an array of objects with color as a hex value',
+        () => expect(
+            R.all(
+                (block) =>
+                    block.color.match(/^#[A-F0-9]{6}$/),
+                convert_to_blocks(char_key, '@ +^')))
+                    .toEqual(true))
+
+    it('should return an array of objects with position as a value of an array of length 2',
+        () => expect(
+            R.all(
+                (block) =>
+                    false,
+                convert_to_blocks(char_key, '@ +^')))
+                    .toEqual(true))
 })
